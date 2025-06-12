b43f2e04c8398f6338db25bbc150319c36fe0c4f
Chaya44099 <c0556744099@gmail.com>
2025-06-12 16:54:17 +0300

    Update Program.cs

--- TeachShareServer/TeachShare/Program.cs
+++ TeachShareServer/TeachShare/Program.cs
@@ -61,7 +61,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("https://teachshare.onrender.com",
             "http://localhost:5173")
               .AllowAnyMethod()
-              .AllowAnyHeader()
+              .AllowAnyHeader();
                   });
 });
 
